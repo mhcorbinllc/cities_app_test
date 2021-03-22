21b7ed2cfd37461610bb6e92cf6868f846b366f2
Dennis <dennisat@users.noreply.github.com>
2021-03-22 06:37:09 +0100

    Update specs

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ export const App = (): JSX.Element => {
       <h1>Cities App <strong>test</strong></h1>
       <blockquote>
         Write an application with a search bar to find a city.<br/>
-        The search bar won't have a <button>Search</button> button, the results will be shown on keypress.<br/>
-        The search bar should always be on top, so on scroll, the user will be able to change the search.<br/>
+        The search bar won't have a <button>Search</button> button. The results will show on keypress.<br/>
+        The search bar should always be on top so that the user can change the search on scroll.<br/>
         The cities are in <code>/src/data/cities.json</code> file.<br/>
-        You are free to use Material UI but is not mandatory.<br/>
+        You are free to use Material UI, but it is not mandatory.<br/>
+        Provide a clean and professional app scaffold and code.
         Push your implementation on a branch.<br/>
       </blockquote>
       <h3>Dev note:</h3>
