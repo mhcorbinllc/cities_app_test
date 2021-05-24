0125d5607d882e1a844d1dfe58318e6e625d3026
Dennis <dennisat@users.noreply.github.com>
2021-05-24 17:42:20 +0200

    Update specs

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ export const App = (): JSX.Element => {
     <div className={styles.root}>
       <h1>Cities App <strong>test</strong></h1>
       <blockquote>
+        <h3>Specs</h3>
         Write an application with a search bar to find a city.<br/>
         The search bar won't have a <button>Search</button> button. The results will show on keypress.<br/>
         The search bar should always be on top so that the user can change the search on scroll.<br/>
         The cities are in <code>/src/data/cities.json</code> file.<br/>
         You are free to use Material UI, but it is not mandatory.<br/>
         Provide a clean and professional app scaffold and code.
-        Push your implementation on a branch.<br/>
+        <h3>Send</h3>
+        Compress to zip acrhive the root folder of this repo without the <code>node_modules</code> folder and email it.<br/>
       </blockquote>
       <h3>Dev note:</h3>
       <p>For new <code>nnn.module.less</code> file, restart the webpack to get the types.</p>
